ff1c00e07b9acde568aeaa4211a01b2ba6c20509
DantesSq <dimonyakovetskyi@gmail.com>
2023-04-11 19:38:09 +0200

    wallet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,40 @@
-import '@component/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@component/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
+
+import { Web3Modal } from '@web3modal/react';
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, goerli } from 'wagmi/chains';
+
+const projectId = '6b8bb11f0139a5d4e9e753fc752d7ac6';
+const chains = [mainnet, goerli];
+
+const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiClient = createClient({
+    autoConnect: true,
+    connectors: w3mConnectors({ version: 1, chains, projectId }),
+    provider,
+});
+
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    const [ready, setReady] = useState(false);
+
+    useEffect(() => {
+        setReady(true);
+    }, []);
+
+    return (
+        <>
+            {ready ? (
+                <WagmiConfig client={wagmiClient}>
+                    <Component {...pageProps} />
+                </WagmiConfig>
+            ) : null}
+
+            <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+        </>
+    );
 }
