0fe8897e5aae443dd44e18d8b3887f947f6073da
DantesSq <dimonyakovetskyi@gmail.com>
2023-04-19 20:43:59 +0200

    list changes

--- components/ListForm.tsx
+++ components/ListForm.tsx
@@ -47,6 +47,11 @@ const ListForm = (props: NftItemInterface) => {
             setError('TokenValuation', { type: 'NaN', message: 'Only numbers are eligible' });
             return;
         }
+        if (isNaN(data.APR)) {
+            setError('APR', { type: 'NaN', message: 'Only numbers are eligible' });
+            return;
+        }
+        alert('You have successfully list your NFT');
         console.log(data);
         // reset();
     };
@@ -96,6 +101,35 @@ const ListForm = (props: NftItemInterface) => {
                 <label>
                     <div className="text-[24px] flex">
                         <input
+                            {...register('APR', {
+                                required: 'Value is required',
+                                valueAsNumber: true,
+                                min: { value: 0.001, message: 'Value should be more' },
+                            })}
+                            placeholder="Loan Amount"
+                            className=" text-[20px] border-[2px] rounded-l-md border-r-0 border-solid border-black pl-2 w-[200px] h-[45px]"
+                        />
+                        <div className="flex items-center justify-center text-[20px] border-[2px] rounded-r-md rounded-sm border-solid border-black pl-2 w-[80px] h-[45px]">
+                            APR, %
+                        </div>
+                    </div>
+                    <p className="text-[#FF0000] text-[14px]">
+                        {errors?.APR && errors.APR.message}
+                    </p>
+                </label>
+                <label>
+                    <select
+                        {...register('Duration')}
+                        className=" text-[20px] border-[2px] rounded-md border-solid border-black pl-2 w-[280px] h-[45px]">
+                        <option value={30}>30 days</option>
+                        <option value={60}>60 days</option>
+                        <option value={90}>90 days</option>
+                        <option value={180}>180 days</option>
+                    </select>
+                </label>
+                <label>
+                    <div className="text-[24px] flex">
+                        <input
                             {...register('InjAddress', {
                                 required: 'Value is required',
                                 valueAsNumber: true,
@@ -108,16 +142,6 @@ const ListForm = (props: NftItemInterface) => {
                     <p className="text-[#FF0000] text-[14px]">
                         {errors?.InjAddress && errors.InjAddress.message}
                     </p>
-                </label>
-                <label>
-                    <select
-                        {...register('Duration')}
-                        className=" text-[20px] border-[2px] rounded-md border-solid border-black pl-2 w-[280px] h-[45px]">
-                        <option value={30}>30 days</option>
-                        <option value={60}>60 days</option>
-                        <option value={90}>90 days</option>
-                        <option value={180}>180 days</option>
-                    </select>
                 </label>
 
                 <input
